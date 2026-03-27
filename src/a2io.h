c763c7c8712dfac9d21b7d190392f149994dfaca
RedLintu16 <redlintu16@gmail.com>
2026-03-27 03:04:07 +0000

    32

--- src/a2io.h
+++ src/a2io.h
@@ -141,7 +141,7 @@
 #  define a2open(path, flags, mode) _wsopen(path, flags, _SH_DENYNO, mode)
 #  define a2fopen(path, mode) _wfsopen(path, mode, _SH_DENYNO)
 // # define a2ftruncate(fd, length): We don't use ftruncate in Mingw build
-#  define a2_off_t off_t
+#  define a2_off_t int64_t
 #elif defined(__ANDROID__) || defined(ANDROID)
 #  define a2lseek(fd, offset, origin) lseek64(fd, offset, origin)
 // # define a2fseek(fp, offset, origin): No fseek64 and not used in aria2
@@ -184,7 +184,7 @@ extern int ftruncate64(int fd, off64_t length);
 #  define a2open(path, flags, mode) open(path, flags, mode)
 #  define a2fopen(path, mode) fopen(path, mode)
 #  define a2ftruncate(fd, length) ftruncate(fd, length)
-#  define a2_off_t off_t
+#  define a2_off_t int64_t
 #endif
 
 #define OPEN_MODE S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
